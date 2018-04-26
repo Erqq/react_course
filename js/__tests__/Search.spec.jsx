f80eb29a815890e79ee578ae08de4bba3658b945
Eerik Timonen <eerik.timonen94@gmail.com>
2018-04-26 09:50:26 +0300

    more tests

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import Search from "../Search";
+import ShowCard from "../ShowCard";
+import preload from "../../data.json";
+import { shallow } from "enzyme";
 
 test("Search renders correctly", () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
+});
+
+test("Search shloud render correct amount of shows", () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+test("Search should render corret amount of shows based on search term", () => {
+  const searchWord = "black";
+  const component = shallow(<Search />);
+
+  component.find("input").simulate("change", { target: { value: searchWord } });
+  const showCount = preload.shows.filter(
+    show =>
+      `${show.title} ${show.description}`
+        .toUpperCase()
+        .indexOf(searchWord.toUpperCase()) >= 0
+  ).length;
+  expect(component.find(ShowCard).length).toEqual(showCount);
 });
