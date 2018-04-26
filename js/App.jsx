c9f879933f5f1beb06c89e7b724e7a007bc388d2
Eerik Timonen <eerik.timonen94@gmail.com>
2018-04-26 10:16:41 +0300

    hmr

---
+++ js/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Landing from "./Landing";
+import Search from "./Search";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+      </div>
+    </BrowserRouter>
+  );
+};
+export default App;
