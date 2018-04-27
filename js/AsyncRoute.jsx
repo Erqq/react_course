03c1806d739ad6a9f2528eaf9124ba284a5684ef
Eerik Timonen <eerik.timonen94@gmail.com>
2018-04-27 16:08:20 +0300

    async route

--- js/AsyncRoute.jsx
+++ js/AsyncRoute.jsx
@@ -5,5 +5,18 @@ class AsyncRoute extends Component {
   state = {
     loaded: false
   };
+  componentDidMount() {
+    this.props.loadingPromise.then(module => {
+      this.component = module.default;
+      this.setState({ loaded: true });
+    });
+  }
+  component = null;
+  render() {
+    if (this.state.loaded) {
+      return <this.component {...this.props} />;
+    }
+    return <Spinner />;
+  }
 }
 export default AsyncRoute;
